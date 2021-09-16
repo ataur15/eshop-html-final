8e6d1dd20bce8b82d321a23f989d55cf716e2731
md-ataur <mdataur15@gmail.com>
2021-09-16 19:15:17 +0600

    new pages add

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,12 @@ export default {
             input: {
                 main: resolve(__dirname, 'index.html'),
                 list: resolve(__dirname, 'list.html'),
-                trading: resolve(__dirname, 'trading.html')
+                trading: resolve(__dirname, 'trading.html'),
+                login: resolve(__dirname, 'login.html'),
+                sell_form: resolve(__dirname, 'sell-form.html'),
+                sell_update_form: resolve(__dirname, 'sell-update-form.html'),
+                buy_form: resolve(__dirname, 'buy-form.html'),
+                buy_update_form: resolve(__dirname, 'buy-update-form.html'),
             }
         }
     }
