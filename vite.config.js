f0334958bbe6a8d383e966c2e847a3096082546f
md-ataur <mdataur15@gmail.com>
2021-10-01 15:17:19 +0600

    new files added

--- vite.config.js
+++ vite.config.js
@@ -17,6 +17,18 @@ export default {
                 sell_update_form: resolve(__dirname, 'sell-update-form.html'),
                 buy_form: resolve(__dirname, 'buy-form.html'),
                 buy_update_form: resolve(__dirname, 'buy-update-form.html'),
+                buyer_invoice: resolve(__dirname, 'buyer-invoice.html'),
+                seller_invoice: resolve(__dirname, 'seller-invoice.html'),
+                sell_order_list: resolve(__dirname, 'sell-order-list.html'),
+                my_buy_order: resolve(__dirname, 'my-buy-order.html'),
+                my_invoice_and_payment: resolve(__dirname, 'my-invoice-and-payment.html'),
+                payment: resolve(__dirname, 'payment.html'),
+                payment_history: resolve(__dirname, 'payment-history.html'),
+                payment_details: resolve(__dirname, 'payment-details.html'),
+                payment_gateway_list: resolve(__dirname, 'payment-gateway-list.html'),
+                payment_gateway_input: resolve(__dirname, 'payment-gateway-input.html'),
+                payment_status: resolve(__dirname, 'payment-status.html'),
+                payment_slip_upload: resolve(__dirname, 'payment-slip-upload.html'),
             }
         }
     }
