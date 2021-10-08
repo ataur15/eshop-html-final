2a36f8b317fd489f2baf7177ad6c7a8638a075a9
md-ataur <mdataur15@gmail.com>
2021-10-08 18:47:52 +0600

    distritc add

--- public/js/tab.js
+++ public/js/tab.js
@@ -7,7 +7,6 @@ const tab = document.querySelectorAll(".tab");
 const panel = document.querySelectorAll(".tab-content");
 
 function onTabClick(event) {
-
     // deactivate existing active tabs and panel
     for (let i = 0; i < tab.length; i++) {
         tab[i].classList.remove("active");
@@ -32,28 +31,57 @@ for (let i = 0; i < tab.length; i++) {
  * Custom tab for top products
  */
 
- const mabs = document.querySelectorAll(".mabs");
- const mab = document.querySelectorAll(".mab");
- const manel = document.querySelectorAll(".mab-content");
+const mabs = document.querySelectorAll(".mabs");
+const mab = document.querySelectorAll(".mab");
+const manel = document.querySelectorAll(".mab-content");
 
- function onMabClick(event) {
-
-     // deactivate existing active mabs and manel
-     for (let i = 0; i < mab.length; i++) {
+function onMabClick(event) {
+    // deactivate existing active mabs and manel
+    for (let i = 0; i < mab.length; i++) {
         mab[i].classList.remove("active");
-     }
+    }
 
-     for (let i = 0; i < manel.length; i++) {
+    for (let i = 0; i < manel.length; i++) {
         manel[i].classList.remove("active");
-     }
+    }
 
-     // activate new mabs and manel
-     event.target.classList.add('active');
-     let classString = event.target.getAttribute('data-target');
-     console.log(classString);
-     document.getElementById('manels').getElementsByClassName(classString)[0].classList.add("active");
- }
+    // activate new mabs and manel
+    event.target.classList.add('active');
+    let classString = event.target.getAttribute('data-target');
+    console.log(classString);
+    document.getElementById('manels').getElementsByClassName(classString)[0].classList.add("active");
+}
 
- for (let i = 0; i < mab.length; i++) {
+for (let i = 0; i < mab.length; i++) {
     mab[i].addEventListener('click', onMabClick, false);
- }
+}
+
+
+/**
+ * Custom tab for 64 districts
+ */
+
+const rabs = document.querySelectorAll(".rabs");
+const rab = document.querySelectorAll(".rab");
+const ranel = document.querySelectorAll(".rab-content");
+
+function onRabClick(event) {
+    // deactivate existing active rabs and ranel
+    for (let i = 0; i < rab.length; i++) {
+        rab[i].classList.remove("active");
+    }
+
+    for (let i = 0; i < ranel.length; i++) {
+        ranel[i].classList.remove("active");
+    }
+
+    // activate new rabs and ranel
+    event.target.classList.add('active');
+    let classString = event.target.getAttribute('data-target');
+    console.log(classString);
+    document.getElementById('ranels').getElementsByClassName(classString)[0].classList.add("active");
+}
+
+for (let i = 0; i < rab.length; i++) {
+    rab[i].addEventListener('click', onRabClick, false);
+}
